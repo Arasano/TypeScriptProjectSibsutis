aa657a9cdd0800b23ac7dcbc613bce3da855e7de
Albert Manankov <modwert404@gmail.com>
2026-02-16 21:54:02 +0700

    Create book part

---
+++ Book.ts
@@ -0,0 +1,25 @@
+interface Book {
+    title: string;
+    author: string;
+    year?: number;
+    genre: 'fiction' | 'non-fiction'; // Union Type — это когда переменная может быть ТОЛЬКО одним из перечисленных значений. Через знак | (или).
+}
+
+function createBook(book: Book): Book {
+    return book;
+}
+
+// С годом
+const book1 = createBook({
+    title: "1984",
+    author: "Orwell",
+    year: 1949,
+    genre: "fiction"
+});
+
+// Без year — это нормально, потому что year?
+const book2 = createBook({
+    title: "Sapiens",
+    author: "Harari",
+    genre: "non-fiction"
+});
